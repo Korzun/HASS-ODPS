55ae1a81a16e8dba08b67deff5a7ef204570febf
Simon Korzun <simon@korzun.com>
2026-04-20 18:24:40 -0400

    chore: rename components folder to component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './theme/theme-provider';
 import { AuthProvider } from './auth/auth-provider';
-import { Header } from './components/header';
-import { LibraryPage } from './components/library-page';
-import { SeriesPage } from './components/series-page';
-import { BookDetailPage } from './components/book-detail-page';
-import { EditMetadataPage } from './components/edit-metadata-page';
+import { Header } from './component/header';
+import { LibraryPage } from './component/library-page';
+import { SeriesPage } from './component/series-page';
+import { BookDetailPage } from './component/book-detail-page';
+import { EditMetadataPage } from './component/edit-metadata-page';
 
 export function App() {
   return (
