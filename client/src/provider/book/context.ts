6b03697eb7af2b7f18c46ff10aa9dce3fe02361c
Simon Korzun <simon@korzun.com>
2026-05-01 15:21:18 -0400

    refactor: add async state to BookContext

--- client/src/provider/book/context.ts
+++ client/src/provider/book/context.ts
@@ -4,10 +4,26 @@ import type { BookList } from './type';
 
 export type BookContext = {
   bookList: BookList;
-  setBookList: (newBookList: BookList) => void;
+  bookListLoading: boolean;
+  bookListError: string | undefined;
+  loadingByBookId: Record<string, boolean>;
+  errorByBookId: Record<string, string | undefined>;
+  setBookList: (updater: (prev: BookList) => BookList) => void;
+  setBookListLoading: (loading: boolean) => void;
+  setBookListError: (error: string | undefined) => void;
+  setLoadingForBook: (bookId: string, loading: boolean) => void;
+  setErrorForBook: (bookId: string, error: string | undefined) => void;
 };
 
 export const Context = createContext<BookContext>({
   bookList: {},
+  bookListLoading: false,
+  bookListError: undefined,
+  loadingByBookId: {},
+  errorByBookId: {},
   setBookList: () => {},
+  setBookListLoading: () => {},
+  setBookListError: () => {},
+  setLoadingForBook: () => {},
+  setErrorForBook: () => {},
 });
