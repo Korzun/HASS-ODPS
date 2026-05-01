bd08e7f2c34f0fea35d206148681a0c319cb5923
Simon Korzun <simon@korzun.com>
2026-05-01 15:10:04 -0400

    refactor: useDeleteUserProgress uses setProgressForUsername

--- client/src/provider/progress/hook/use-delete-user-progress.ts
+++ client/src/provider/progress/hook/use-delete-user-progress.ts
@@ -1,36 +1,33 @@
 import { useCallback, useContext, useMemo, useState } from 'react';
 
 import { Context } from '../context';
-import { UserProgressList } from '../type';
+import type { UserProgressList } from '../type';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const removeProgressById = (bookId: string, {[bookId]: _, ...rest}: UserProgressList) => rest;
+const removeProgressById = (bookId: string, { [bookId]: _, ...rest }: UserProgressList) => rest;
 
-export type DeleteUserProgress = (bookId: string) => Promise<void>
+export type DeleteUserProgress = (bookId: string) => Promise<void>;
 export type UseDeleteUserProgress =
-  | [DeleteUserProgress, false, false, undefined]  // Initial State
-  | [DeleteUserProgress, true, false, undefined]   // Progress is being deleted
-  | [DeleteUserProgress, false, true, undefined]   // There was an unspecified error while deleting progress
-  | [DeleteUserProgress, false, true, string];     // There was a specified error while deleting progress
+  | [DeleteUserProgress, false, false, undefined]
+  | [DeleteUserProgress, true, false, undefined]
+  | [DeleteUserProgress, false, true, undefined]
+  | [DeleteUserProgress, false, true, string];
+
 export const useDeleteUserProgress = (username?: string): UseDeleteUserProgress => {
-  const { progressList, setProgressList } = useContext(Context);
+  const { progressList, setProgressForUsername } = useContext(Context);
   const [deleting, setDeleting] = useState(false);
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string | undefined>();
 
   const deleteUserProgress = useCallback(async (bookId: string) => {
-    // Prevent multiple parallel requests 
-    if(deleting) {
-      return;
-    }
+    if (deleting) return;
 
-    if(username === undefined) {
+    if (username === undefined) {
       setError(true);
       setErrorMessage('Failed to delete progress');
       return;
     }
 
-    // Prevent calling the API if the user & book progress doesn't exist
     const userProgressList = progressList[username];
     const progress = userProgressList?.[bookId];
     if (progress === undefined) {
@@ -39,11 +36,7 @@ export const useDeleteUserProgress = (username?: string): UseDeleteUserProgress 
       return;
     }
 
-    // Optimistically remove progress
-    setProgressList({
-      ...progressList,
-      [username]: removeProgressById(bookId, userProgressList)
-    });
+    setProgressForUsername(username, removeProgressById(bookId, userProgressList));
 
     try {
       setDeleting(true);
@@ -51,24 +44,17 @@ export const useDeleteUserProgress = (username?: string): UseDeleteUserProgress 
       setErrorMessage(undefined);
       const response = await fetch(
         `/api/users/${encodeURIComponent(username)}/progress/${encodeURIComponent(bookId)}`,
-        { method: 'DELETE' }
+        { method: 'DELETE' },
       );
-      if (response.status !== 204) {
-        throw new Error('Failed to clear progress');
-      }
-    } catch (error) {
+      if (response.status !== 204) throw new Error('Failed to clear progress');
+    } catch (err) {
       setError(true);
-      setProgressList({
-        ...progressList,
-        [username]: {...userProgressList, [bookId]: progress }
-      });
-      if (error instanceof Error) {
-        setErrorMessage(error.message);
-      }
+      setProgressForUsername(username, { ...userProgressList, [bookId]: progress });
+      if (err instanceof Error) setErrorMessage(err.message);
     } finally {
       setDeleting(false);
     }
-  }, [progressList, setProgressList]);
+  }, [progressList, setProgressForUsername, username, deleting]);
 
   return useMemo(
     () => [deleteUserProgress, deleting, error, errorMessage] as UseDeleteUserProgress,
