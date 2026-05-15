f9ce98d2b9cb937831785ac29a686a596e195b37
Simon Korzun <simon@korzun.com>
2026-05-15 17:18:14 -0400

    feat: add field-list style

---
+++ client/src/control/field-list/style.ts
@@ -0,0 +1,22 @@
+import { createUseStyles, type Theme } from '~/provider/theme';
+
+export const useStyle = createUseStyles((_theme: Theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '12px',
+  },
+  rowContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '0.2rem',
+  },
+  row: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '4px',
+  },
+  field: {
+    flexGrow: 1,
+  },
+}));
